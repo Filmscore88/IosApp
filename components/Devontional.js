adff40016353c2b72c247916a96683c263a30ec3
filmscore88 <cmtmorales24@gmail.com>
2020-03-17 11:21:39 -0500

    created devotional component for newsReel usage

---
+++ components/Devontional.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  ScrollView,
+  Image,
+  FlatList
+} from "react-native";
+
+function Devotional() {}
+
+export default Devotional;
