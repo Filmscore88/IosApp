b423ff6284f4b0be5196dd218106a6ad6973187a
filmscore88 <cmtmorales24@gmail.com>
2020-03-18 13:51:18 -0500

    changed scrollview to flatlist to render data

--- components/Events.js
+++ components/Events.js
@@ -4,12 +4,21 @@ import {
   View,
   Text,
   Dimensions,
-  ScrollView,
   Image,
   FlatList
 } from "react-native";
 
-function Events() {
+const { width } = Dimensions.get("window");
+
+renderRow = ({ item }) => {
+  return (
+    <View style={styles.itemRow}>
+      <Image source={item.thumbnailUrl} style={styles.itemImage} />
+      <Text style={styles.itemText}>{item.name}</Text>
+    </View>
+  );
+};
+const Events = () => {
   const [events, setData] = useState([
     {
       name: "Financial Peace University Class",
@@ -24,6 +33,110 @@ function Events() {
       url: "https://www.facebook.com/events/865284773919966/"
     }
   ]);
-}
 
+  return (
+    <FlatList
+      style={styles.contaner}
+      data={events}
+      renderItem={this.renderRow}
+      keyExtractor={(item, index) => index.toString()}
+      onReachedThreshold={0}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    backgroundColor: "#f5fcff"
+  },
+  itemRow: {
+    borderBottomColor: "#ccc",
+    marginBottom: 10,
+    borderBottomWidth: 1
+  },
+  itemText: {
+    fontSize: 16,
+    padding: 5
+  },
+  itemImage: {
+    width: "100%",
+    height: 200,
+    resizeMode: "cover"
+  },
+  loader: {
+    marginTop: 10,
+    alignItems: "center"
+  }
+});
 export default Events;
+
+//
+// <ScrollView
+//   style={{flex: 1}}
+//   contentContainerStyle={{width: '100%', alignItems: 'center'}}>
+//
+// {this.state.test.map((item, index) => {
+//   <View style={{height: 50, width: 50, backgroundColor: 'orange', marginBottom: 10}} />
+// })}
+//
+//   <SomeOtherComponent />
+// </ScrollView>
+//
+// ---------------------------------------------------------------------------------
+// const { width } = Dimensions.get('window');
+//
+// export default class App extends Component {
+//
+//   componentDidMount() {
+// 		setTimeout(() => {this.scrollView.scrollTo({x: -30}) }, 1) // scroll view position fix
+// 	}
+//
+//   render() {
+//     return (
+// <ScrollView
+//   ref={(scrollView) => { this.scrollView = scrollView; }}
+//   style={styles.container}
+//   //pagingEnabled={true}
+//   horizontal= {true}
+//   decelerationRate={0}
+//   snapToInterval={width - 60}
+//   snapToAlignment={"center"}
+//   contentInset={{
+//     top: 0,
+//     left: 30,
+//     bottom: 0,
+//     right: 30,
+//   }}>
+//   <View style={styles.view} />
+//   <View style={styles.view2} />
+//   <View style={styles.view} />
+//   <View style={styles.view2} />
+//   <View style={styles.view} />
+// </ScrollView>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {},
+//   view: {
+//     marginTop: 100,
+//     backgroundColor: 'blue',
+//     width: width - 80,
+//     margin: 10,
+//     height: 200,
+//     borderRadius: 10,
+//     //paddingHorizontal : 30
+//   },
+//   view2: {
+//     marginTop: 100,
+//     backgroundColor: 'red',
+//     width: width - 80,
+//     margin: 10,
+//     height: 200,
+//     borderRadius: 10,
+//     //paddingHorizontal : 30
+//   },
+// });
+//
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import NewsReel from "../components/NewsReel";
+import Events from "../components/Events";
 function Home() {
   return (
     <View>
       <Text>This is the Home Screen where you can scroll the newsreel</Text>
 
-      <NewsReel />
+      <Events />
     </View>
   );
 }
