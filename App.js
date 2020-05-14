aab197f8c9c07b60f2c0e3cf3d75282b1152d60f
filmscore88 <cmtmorales24@gmail.com>
2020-05-14 14:28:07 -0500

    added firebase js sdk to project

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 import HomeStack from "./components/HomeStack";
 import LiveBackground from "./components/LiveBackground";
 import Quote from "./components/Quote";
+
 export default function App() {
   return (
     <View>
