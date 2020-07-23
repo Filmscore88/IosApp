b18f4896cb8373b81701e3c4af17bd1a21ccbba7
filmscore88 <cmtmorales24@gmail.com>
2020-07-23 14:29:39 -0500

    full screen modal for rendering screens

--- components/LabelBottomNavigation.js
+++ components/LabelBottomNavigation.js
@@ -1,27 +1,30 @@
 import React from "react";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { NavigationContainer } from "@react-navigation/native";
 import Give from "../screens/Give";
 import Sermons from "../screens/Sermons";
 import ImgBackground from "../components/ImgBackground";
-const Tab = createMaterialBottomTabNavigator();
+import { View } from "react-native";
+
+const Tab = createBottomTabNavigator();
 
 export default function LabelBottomNavigation() {
+  const CreatePlaceholder = () => (
+    <View style={{ flex: 1, backgroundColor: "blue" }} />
+  );
   return (
     <NavigationContainer>
       <Tab.Navigator
-        barStyle={{ backgroundColor: "#694fad" }}
-        initialRouteName="Feed"
+        initialRouteName="ImgBackground"
         inactiveColor="#3e2465"
         activeColor="#e91e63"
         style={{ backgroundColor: "tomato" }}
       >
         <Tab.Screen
-          name="Categories"
+          name="ImgBackground"
           component={ImgBackground}
           options={{
-            tabBarLabel: "Home",
             tabBarIcon: ({ color }) => (
               <MaterialCommunityIcons name="home" color={color} size={26} />
             )
@@ -29,9 +32,8 @@ export default function LabelBottomNavigation() {
         />
         <Tab.Screen
           name="Music"
-          component={Sermons}
+          component={CreatePlaceholder}
           options={{
-            tabBarLabel: "Updates",
             tabBarIcon: ({ color }) => (
               <MaterialCommunityIcons name="bell" color={color} size={26} />
             )
